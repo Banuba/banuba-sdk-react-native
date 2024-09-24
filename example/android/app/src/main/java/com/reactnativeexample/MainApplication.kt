5b16f5fa9dc569963f195823394373e4765bc85c
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2024-09-24 17:03:46 +0300

    update react-native to version 0.73.9

--- example/android/app/src/main/java/com/reactnativeexample/MainApplication.kt
+++ example/android/app/src/main/java/com/reactnativeexample/MainApplication.kt
@@ -17,14 +17,12 @@ class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> {
+        override fun getPackages(): List<ReactPackage> = 
+        PackageList(this).packages.apply {
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          val packages = PackageList(this).packages
-          packages.add(RNFSPackage())
-          return packages
+          // add(MyReactNativePackage())
+          add(RNFSPackage())
         }
-
         override fun getJSMainModuleName(): String = "index"
 
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
